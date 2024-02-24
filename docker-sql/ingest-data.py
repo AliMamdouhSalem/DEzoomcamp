48fe9ea0d26db3ba677afa1a0dd7b3d4b6a8fa54
AliMamdouhSalem <74428524+AliMamdouhSalem@users.noreply.github.com>
2024-02-24 23:54:32 +0000

    modified:   docker-sql/Dockerfile 	new file:   docker-sql/ingest-data.py 	new file:   docker-sql/yellow_tripdata_2021-01.csv 	new file:   important commands.md 	deleted:    yellow_tripdata_2021-01.csv.gz

---
+++ docker-sql/ingest-data.py
@@ -0,0 +1,85 @@
+
+import os
+import argparse
+
+import pandas as pd
+from sqlalchemy import create_engine
+from time import time
+
+
+
+def main(params):
+    user=params.user
+    password=params.password
+    host=params.host
+    port=params.port
+    db=params.db
+    table_name=params.table_name
+    url= params.url
+    csv_name='output.csv.gz'
+    
+    os.system(f"wget {url} -O {csv_name}")
+    engine= create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
+    engine.connect()
+
+    df_iter= pd.read_csv(csv_name, iterator=True, chunksize=100000, low_memory=False, encoding='UTF')
+    df=next(df_iter)
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    while True:
+        t_start= time()
+        df=next(df_iter)
+        
+        df.tpep_pickup_datetime=pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime=pd.to_datetime(df.tpep_dropoff_datetime)
+        
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+
+        t_end=time()
+        print('inserted another chunck..., took %.3f second' %(t_end-t_start))
+
+
+if __name__== '__main__':
+    parser= argparse.ArgumentParser(description='ingest CSV data to postgres')
+
+    #user 
+    #password 
+    #host 
+    #port 
+    #database name 
+    #table name 
+    #url
+
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database for postgres')
+    parser.add_argument('--table_name', help='table name for postgres')
+    parser.add_argument('--url', help='table name for url')
+    
+    args=parser.parse_args()
+    main(args)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+# In[ ]:
+
+
+
+
